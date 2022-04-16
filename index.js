3ff12670ed41a4666de58acf6420f545d3a1bce3
Amjad Alkhatib <amjad.mo.kh@gmail.com>
2022-04-16 03:29:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+
+
+// function superbowlWin(record){
+//     return record.find((record) => {if(record.result === "W") {
+//         return record.year;
+//     } else return undefined;
+// });
+// }
+
+function superbowlWin(record){
+    let Winner = record.find((record) => (record.result === "W"));
+    return Winner? Winner.year: undefined;
+}
